c052cf1d5dd0720fa90fc762dfb9ad6707607642
ChewCEC <camilo.echeverri.100@gmail.com>
2022-09-24 13:29:24 -0500

    3
    zoo y zona

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -13,3 +13,40 @@ public class Zona {
 		this.nombre = nombre;
 		zoo = lugar;
 	}
+	
+	public Zona() {
+		
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Zoologico getZoo() {
+		return zoo;
+	}
+
+	public void setZoo(Zoologico zoo) {
+		this.zoo = zoo;
+	}
+
+	public ArrayList<Animal> getAnimales() {
+		return animales;
+	}
+
+	public void setAnimales(ArrayList<Animal> animales) {
+		this.animales = animales;
+	}
+
+	public void agregarAnimales(Animal especie) {
+		animales.add(especie);
+	}
+	
+	public int cantidadAnimales() {
+		return animales.size();
+	}
+}
--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -12,5 +12,46 @@ public class Zoologico {
 		this.nombre = nombre;
 		this.ubicacion = ubicacion;
 	}
+	
+	public Zoologico() {
+		this(null, null);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public ArrayList<Zona> getZona() {
+		return zonas;
+	}
+
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+
+	public void agregarZonas(Zona zona) {
+		zonas.add(zona);
+	}
+	
+	public int cantidadTotalAnimales() {
+		int total = 0;
+		for (int i = 0; i<zonas.size(); i++) {
+			total = total + zonas.get(i).cantidadAnimales();
+		
+		}
+		return total;
+	}
+	
 }
