b0217bc2648b51b0f15a4207e8965ba9ac5f22c6
ChewCEC <camilo.echeverri.100@gmail.com>
2022-09-24 13:08:06 -0500

    :+1:

---
+++ src/main/java/Ave.java
@@ -0,0 +1,11 @@
+package zooAnimales;
+
+import gestion.Zona;
+
+import java.util.ArrayList;
+
+public class Ave extends Animal{
+    private static ArrayList<Ave> listado = new ArrayList<>();
+    public static int halcones;
+    public static int aguilas;
+    private String colorPlumas;
